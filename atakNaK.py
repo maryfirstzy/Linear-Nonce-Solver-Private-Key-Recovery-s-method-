211658a9de66d1f3d1a67a195ad748aef1cf4aac
maryfirstzy <maryfirst865@gmail.com>
2026-07-09 23:08:02 +0100

    Update atakNaK.py

--- atakNaK.py
+++ atakNaK.py
@@ -2,12 +2,12 @@ from ecdsa.numbertheory import inverse_mod
 from ecdsa.ecdsa import generator_secp256k1
 
 # Dane z transakcji
-r1 = int("baf2aa695873ee637a1b23b53a78c512e4ea8ed72738badbdac3fe6b2a769176", 16)
-r2 = int("d56a07721d620e6b3c64021713ae2cec9bc831c4d6d32501e347142bff70d078", 16)
-s1 = int("b47bbe4d2e405452dfa95bbd6ac3804c38c25f838edafd5ceb3456f3b040b0a6", 16)
-s2 = int("4c136bac45a92b2adfc0af27282b494f6dc416535433d36e04057de2bf7cc326", 16)
-z1 = int("d20aff079cd86074eff889e1f4f0fbd0b97ef4eeff378147afc815d8a28552d5", 16)
-z2 = int("17ea532a30334538c281467befb5fca7e66c6b1e760f5d17d77cb853c65a3c7d", 16)
+r1 = int("f06c2455e7fc6e4bcc73ce05a4de8906794082e4ba298d4647c50a7d6ebfe988", 16)
+r2 = int("1cd5156d9a3eb0fca2e28244467cb29af6cf5617ba3be9e277879e3ad6421de1", 16)
+s1 = int("f2ad6ad6791612a5c3a5094c358ee459dbad1821345a008dc9856feb7d142036", 16)
+s2 = int("eb4b452ed360722c9b9b2050e306ed1195e66ed16355c9c95792a3c4701e270c", 16)
+z1 = int("bcf16bdfc712aeb511592ecc73d9c5a4c3a756307d9c00e3a8ed31f7b5bf90c7", 16)
+z2 = int("6cd6b4607b73d5933de63f7f3abe7df79bebb13df549eb9c686d7770784c8661", 16)
 
 # Moduł porządkowy krzywej secp256k1
 n = generator_secp256k1.order()
